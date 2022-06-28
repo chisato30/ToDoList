892e58e779d7f11d56c433d2dacbacaf8df786bb
chisato30 <moterunit@gmail.com>
2022-06-28 19:16:32 +0900

    buildgradle追記

--- app/src/main/java/database/ToDo.kt
+++ app/src/main/java/database/ToDo.kt
@@ -1,11 +1,28 @@
 package database
 
-
+@Entity(tableName = "todo_table")
 data class ToDo(
+    @PrimaryKey(autoGenerate = true)
     var toDoId: Long = 0L,
+
+    @ColumnInfo(name = "title")
     var title: String,
+
+    @ColumnInfo(name = "choice_time")
     var choiceTime: Long,
+
+    @ColumnInfo(name = "choice_date")
     var choiceDate: Long,
+
+    @ColumnInfo(name = "detail")
     var detail: String,
+
+    @ColumnInfo(name = "schedule")
     var schedule: Int = -1,
 )
+
+annotation class ColumnInfo(val name: String)
+
+annotation class PrimaryKey(val autoGenerate: Boolean)
+
+annotation class Entity(val tableName: String)
